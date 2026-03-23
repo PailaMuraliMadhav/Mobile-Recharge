ca3edbeb84b764fbca77ea803e207dc3ab47b034
Paila Murali Madhav <muralipaila1313@gmail.com>
2026-03-23 15:19:58 +0530

    some changes made in exceptions.

--- User-Service/src/main/java/com/example/User_Service/Dto/LoginResponse.java
+++ User-Service/src/main/java/com/example/User_Service/Dto/LoginResponse.java
@@ -3,8 +3,7 @@ package com.example.User_Service.Dto;
 import com.example.User_Service.Enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+
 
 @Data
 @AllArgsConstructor
