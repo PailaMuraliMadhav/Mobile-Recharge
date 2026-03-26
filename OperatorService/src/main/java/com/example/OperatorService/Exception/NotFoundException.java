d9451bbaf81f68dc7fe04e7240ca5575af7e2ff6
Paila Murali Madhav <muralipaila1313@gmail.com>
2026-03-26 23:43:49 +0530

    addded  opetator service

--- OperatorService/src/main/java/com/example/OperatorService/Exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.OperatorService.Exception;
-
-
-public class NotFoundException  extends RuntimeException{
-    public  NotFoundException(String message){
-        super(message);
-    }
-
-}
